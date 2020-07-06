222b8defbaf8a9dd01d16feb5eb267aab6f99675
ubuntux92 <67382068+ubuntux92@users.noreply.github.com>
2020-07-06 23:35:19 +0700

    Create form_delete.php

---
+++ form_delete.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<h1>Form Delete</h1>
+</head>
+<body>
+<p><a href='index.php'>Kembali ke Halaman Utama</a></p>
+<form action="process_form_delete.php" method="post">
+<input type="text" name="id" placeholder="id">
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
