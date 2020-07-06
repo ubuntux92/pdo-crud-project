74b75580cf0eef0e746f1e7bb9bb6d5a7da4fd05
ubuntux92 <67382068+ubuntux92@users.noreply.github.com>
2020-07-06 23:32:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<p>Menu</p>
+<ul>
+<li><a href="form_input.php">Form Input</a></li>
+<li><a href="form_update.php">Form Update</a></li>
+<li><a href="form_delete.php">Form Delete</a></li>
+<li><a href="show_data.php">Show Data</a></li>
+</ul>
+</body>
+</html>
