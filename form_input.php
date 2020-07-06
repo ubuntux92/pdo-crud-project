d50e7a89e29ba6e86fda5b09a1489aebda069f7b
ubuntux92 <67382068+ubuntux92@users.noreply.github.com>
2020-07-06 23:33:18 +0700

    Create form_input.php

---
+++ form_input.php
@@ -0,0 +1,12 @@
+<head>
+<h1>Form Input</h1>
+</head>
+<body>
+<p><a href='index.php'>Kembali ke Halaman Utama</a></p>
+<form action="process_form_input.php" method="post">
+<input type="text" name="email" placeholder="email">
+<input type="password" name="password" placeholder="password">
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
