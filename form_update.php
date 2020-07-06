9fcb46101d2e4fea5e667de3c08346beb8a75dc9
ubuntux92 <67382068+ubuntux92@users.noreply.github.com>
2020-07-06 23:34:16 +0700

    Create form_update.php

---
+++ form_update.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<h1>Form Update</h1>
+</head>
+<body>
+<p><a href='index.php'>Kembali ke Halaman Utama</a></p>
+<form action="process_form_update.php" method="post">
+<input type="text" name="id" placeholder="id">
+<input type="text" name="email" placeholder="email">
+<input type="password" name="password" placeholder="password">
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
